3f4589e22156b9719c7c01cbc65b40eb4717b689
Itay Zemah <itay@powerlink.co.il>
2023-05-04 18:15:33 +0300

    2

--- src/init.adapter.ts
+++ src/init.adapter.ts
@@ -1,10 +1,11 @@
 import { ToDoDao } from "./dao/todo";
+import { ApiToDoDao } from "./dao/todo/api.todo.dao";
 import { KnexToDoDao } from "./dao/todo/knex.todo.dao";
 import { KnexUserDao, UserDao } from "./dao/user";
 
 export function initAdapters(): Adapters {
   const userDao = new KnexUserDao();
-  const toDoDao = new KnexToDoDao();
+  const toDoDao = new ApiToDoDao(); //new KnexToDoDao();
   return { userDao, toDoDao };
 }
 export type Adapters = {
